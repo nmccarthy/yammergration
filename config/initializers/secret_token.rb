2b2758546a50a3bb2bcb7fc84d31aaa02f07853f
Neil McCarthy <nmccarthy@yammer-inc.com>
2012-10-23 19:43:35 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yammergration::Application.config.secret_token = 'cde91b67e675116ef983a7015963161162db9afd0b1e856ea93da9eb8ef57d8fcd18ec4a41e1efca2042190445ca28f702cc5d564b5d404fc7f1a58749dab2e0'
